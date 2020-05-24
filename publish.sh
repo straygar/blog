19fc90a6b14d098d94f9bac369737fc2a55ee31c
Aigars Reiters <straygar@gmail.com>
2020-05-24 22:30:18 +0100

    Add hacky publish script

---
+++ publish.sh
@@ -0,0 +1,21 @@
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+#!/bin/bash
+# Generate updates
+hugo
+
+# Publish static files
+cd straygar.github.io
+git add .
+git commit -m $1
+git push
+
+# Publish Hugo repo updates
+cd ..
+git add .
+git commit -m $1
+git push 
